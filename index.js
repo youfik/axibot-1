1c622eb071abe9b8a40cbe97499000de821fdbc0
VeraSploit <gkdlpjyjp0ct@opayq.com>
2018-10-11 09:46:04 -0800

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const config = require("./lizardcon.json")
+const config = require("./Axiacon.json")
 const nodeyourmeme = require('nodeyourmeme');
 const getMemeUrls = require('get-meme-urls');
 var request = require('request');
 var randomCat = require('random-cat');
+var querystring = require("querystring");
+const { dataClasses, search } = require('hibp');
+const hibp = require('hibp');
+const math = require('mathjs');
+const encodeUrl = require("encodeurl");
+const dogPicture = require("random-puppy");
+const aes = require("aes");
+
+bot.on("error", (e) => console.error(e));
+bot.on("warn", (e) => console.warn(e));
+bot.on("debug", (e) => console.info(e));
 
 bot.on('ready', () => {
   console.log('Connected to discord.');
-  console.log('Lizard');
+  console.log('Axia');
   console.log('Made by Etern4lSec // Youri');
 });
 
@@ -24,15 +35,30 @@ bot.on('ready', () => {
 
 
 function doMagic8BallVoodoo() {
-    var rand = [':8ball: Absolutly.', ':8ball: Absolutly not.', ':8ball: It is true.', ':8ball: Impossible.', ':8ball: Of course.', ':8ball: I do not think so.', ':8ball: It is true.', ':8ball: It is not true.', ':8ball: I am very undoubtful of that.', ':8ball: I am very doubtful of that.', ':8ball: Sources point to no.', ':8ball: Theories prove it.', ':8ball: Reply hazy try again', ':8ball: Ask again later', ':8ball: Better not tell you now', ':8ball: Cannot predict now', ':8ball: Concentrate and ask again'];
+    var rand = [':8ball: Absoluut.', ':8ball: Absoluut niet.', ':8ball: Het is echt zo.', ':8ball: Kan niet.', ':8ball: Natuurlijk.', ':8ball: Ik denk het niet.', ':8ball: Het is waar.', ':8ball: Het is niet waar.', ':8ball: Ik betwijfel het.', ':8ball: Ik betwijfel het ten zeerste.', ':8ball: Mijn bronnen zeggen van niet.', ':8ball: StriktGeheim zegt van wel.', ':8ball: Ik ben in de war. Probeer het opnieuw.', ':8ball: Daar ga ik nu geen antwoord op geven.', ':8ball: Dat wil je niet weten.', ':8ball: Ik weet het niet zeker.', ':8ball: Concentreer je, en vraag het opnieuw.'];
 
     return rand[Math.floor(Math.random()*rand.length)];
 }
 
 function tossThisCoin() {
-    var rand = ['You tossed a coin, it lands on heads.', 'You tossed a coin, it lands on tails.'];
+    var rand = ['Je gooide een muntje. Het is kop.', 'Je gooide een muntje. Het is munt.'];
     return rand[Math.floor(Math.random()*rand.length)];
 }
+
+bot.on('message', msg => {
+	if ( !msg.guild.id ) {
+	return;
+	}
+	else {
+		let substr = "@Axia College#5796";
+		if (msg.isMentioned(bot.user)) {
+			msg.reply("hai. Ik ben de Axia Bot. Gebruik !help voor een lijst met mijn commando's.");
+		}
+			else {
+			return;
+		}
+	}
+});
 
 bot.on('message', async msg => {
   if (msg.author.bot) return;
@@ -42,45 +68,38 @@ bot.on('message', async msg => {
   }
   let command = msg.content.split(" ")[0];
   command = command.slice(config.prefix.length);
-  console.log('The ' + command + ' command has been used in the server with ID ' + msg.guild.id + '.');
+  console.log('Iemand voerde ' + command + ' in de axia server. Voor verificatie is hier het Guild ID: ' + msg.guild.id + '.');
 
   let args = msg.content.split(" ").slice(1);
   if (command === "author") {
-	  //msg.channel.send("The Lizard bot is made by Etern4lSec, also known as JustEtern4l. His real name is Youri Kuilman. Youri is a 14 year old developer from The Netherlands. Besides programming, he also writes articles on **https://www.yourikuilman.nl**. You can follow him on Twitter too: `@JustEtern4l`.")
-	  msg.channel.send("The Lizard bot is made by Etern4lSec, also known as JustEtern4l. His real name is Youri Kuilman. Youri is a 14 year old developer from The Netherlands. He would love to hear your feedback, so (if you have any) contact `Etern4lSec#7923`.")
+	  msg.channel.send("Hallo! Mijn naam is Youri, en ik ben een leerling op het Axia College. Ik programmeer veel in mijn vrije tijd, en maak (zoals je ziet) ook discord bots in Node.JS (een programmeertaal). Ik heb deze bot ooit ontwikkeld als public bot, onder de naam Lizard. Maar nu gebruik ik de code nergens voor, dus maakte ik er maar deze Axia bot van.");
 }
 
   else if (command === "ping") {
-    msg.channel.send("Pong! (waiting for response...)").then(m => m.edit(`Pong! (Current latency is ${m.createdTimestamp - msg.createdTimestamp}ms, while the API Latency is ${Math.round(bot.ping)}ms)`) );
+    msg.channel.send("Pong! (even geduld, latency...)").then(m => m.edit(`Pong! (Latency is ${m.createdTimestamp - msg.createdTimestamp}ms, en API latency is ${Math.round(bot.ping)}ms)`) );
   }
 
   else if (command === "pong") {
-     msg.channel.send("Ping! (waiting for response...)").then(m => m.edit(`Ping! (Current latency is ${m.createdTimestamp - msg.createdTimestamp}ms, while the API Latency is ${Math.round(bot.ping)}ms)`) );
+     msg.channel.send("Ping! (even geduld, latency...)").then(m => m.edit(`Ping! (Latency is ${m.createdTimestamp - msg.createdTimestamp}ms, en API latency is ${Math.round(bot.ping)}ms)`) );
 
   }
 
   else if (command === "avatar") {
 	var otherAvatarMention = msg.mentions.users.first();
 	let substr = "@";
-	if ( typeof otherAvatarMention !== 'undefined' && otherAvatarMention )
-	{
-		msg.reply(otherAvatarMention.avatarURL);
+	if(msg.content.indexOf(substr) > -1) {
+		msg.reply("mention een gebruiker op deze server.");
 	}
-	else if(msg.content.indexOf(substr) > -1) {
-		msg.reply("mention a valid user.");
-	}
-	else if ( otherAvatarMention )
+	else if ( !otherAvatarMention )
 	{
 		msg.reply(msg.author.avatarURL);
+	}
+	else {
+		msg.reply(otherAvatarMention.avatarURL);
 	}
   }
   else if (command === "8ball") {
     msg.channel.send(doMagic8BallVoodoo())
-  }
-
-  else if (command === "invite") {
-    msg.reply("I've sent you a DM with the invite link for Lizard.")
-    msg.author.send("The invite link for the Lizard bot is: **https://bit.ly/2JE4bfU**")
   }
 
   else if (command === "toss") {
@@ -88,85 +107,77 @@ bot.on('message', async msg => {
   }
   
   else if(command === "kick") {
-    if(!msg.member.roles.some(r=>["Moderator", "Helper"].includes(r.name)) )
-      return msg.reply("Sorry, you don't have permissions to use this!");
+    if(!msg.member.roles.some(r=>["Beheerders"].includes(r.name)) )
+      return msg.reply("leuke poging. Alleen Beheerders mogen dit.");
     let member = msg.mentions.members.first() || msg.guild.members.get(args[0]);
     if(!member)
-      return msg.reply("Please mention a valid member of this server.");
+      return msg.reply("mention een gebruiker op deze server.");
     if(!member.kickable) 
-      return msg.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      return msg.reply("Ik kan deze gebruiker niet kicken, ze hebben misschien hogere permissies dan de Axia bot. Ik kan alleen Leerlingen kicken.");
     let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+    if(!reason) reason = "Je bent verbannen.";
     member.kick(reason)
-      .catch(error => msg.reply(`Sorry ${msg.author} I couldn't kick because of : ${error}`));
-    msg.reply(`${member.user.tag} has been kicked by ${msg.author.tag} because: ${reason}`);
+      .catch(error => msg.reply(`sorry ${msg.author}. Ik krijg deze error : ${error}. Meld dit anders even bij Youri`));
+    msg.reply(`${member.user.tag} is gekicked door ${msg.author.tag}. Reden: ${reason}`);
 
   }
   
   else if(command === "ban") {
-    if(!msg.member.roles.some(r=>["Moderator"].includes(r.name)) )
-      return msg.reply("Sorry, you don't have permissions to use this!");
+    if(!msg.member.roles.some(r=>["Beheerders"].includes(r.name)) )
+      return msg.reply("leuke poging. Alleen Beheerders mogen dit.");
     
     let member = msg.mentions.members.first();
     if(!member)
-      return msg.reply("Please mention a valid member of this server");
+      return msg.reply("mention een gebruiker op deze server.");
     if(!member.bannable) 
-      return msg.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+      return msg.reply("Ik kan deze gebruiker niet bannen, ze hebben misschien hogere permissies dan de Axia bot. Ik kan alleen Leerlingen bannen.");
 
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason provided";
     
     member.ban(reason)
-      .catch(error => msg.reply(`Sorry ${msg.author} I couldn't ban because the following error occured: ${error}`));
-    msg.reply(`${member.user.tag} has been banned by ${msg.author.tag} for: ${reason}`);
+      .catch(error => msg.reply(`sorry ${msg.author}. Ik krijg deze error : ${error}. Meld dit anders even bij Youri`));
+    msg.reply(`${member.user.tag} is gebanned door ${msg.author.tag}. Reden: ${reason}`);
   }
   
   else if(command === "clear") {
+    if(!msg.member.roles.some(r=>["Beheerders"].includes(r.name)) )
+      return msg.reply("leuke poging. Alleen Beheerders mogen dit.");
     const deleteCount = parseInt(args[0], 10);
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return msg.reply("Please provide a number between 2 and 100 as the amount of messages to delete.");
+      return msg.reply("geef een nummer van 2 tot en met 100 op als hoeveelheid.");
     const fetched = await msg.channel.fetchMessages({limit: deleteCount});
     msg.channel.bulkDelete(fetched)
-      .catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));
+      .catch(error => msg.reply(`sorry ${msg.author}. Ik krijg deze error : ${error}. Meld dit anders even bij Youri`));
   }
   
   else if (command === "meme") {
 	if ( typeof args[0] !== 'undefined' && args[0] )
 		getMemeUrls(args[0]).then(result => {
 			var response = result[ Math.random() * result.length |0 ];
-			let embed = new Discord.RichEmbed()
-			.setImage(response)
-			.setColor("#36393E")
-			.setFooter("Made possible by memegenerator.com");
-			
-			msg.channel.send({embed})
+			msg.channel.send(response + "\n*Met dank aan memegenerator.com*");
   })
 	else
 		getMemeUrls('discord').then(result => {
 			var response = result[ Math.random() * result.length |0 ];
-			let embed = new Discord.RichEmbed()
-			.setImage(response)
-			.setColor("#36393E")
-			.setFooter("Made possible by memegenerator.com");
-			
-			msg.channel.send({embed})
+			msg.channel.send(response + "\n*Met dank aan memegenerator.com*");
   })
 }
 
-  else if (command === "cat") {
+  else if (command === "kat") {
 	var url = randomCat.get({
 	category: 'cats'
 	});
-    msg.channel.send(url);
+	msg.channel.send(url + "\n*Met dank aan lorempixel.com*");
   }
 
-  else if (command === "food") {
+  else if (command === "voedsel") {
 	var foodUrl = randomCat.get({
 	category: 'food'
 });
-	msg.channel.send(foodUrl);
+	msg.channel.send(foodUrl + "\n*Met dank aan lorempixel.com*");
   }
-  
+
   else if(command === "userinfo") {
     let user;
   
@@ -181,31 +192,88 @@ bot.on('message', async msg => {
       .setThumbnail(user.avatarURL)
       .setTitle(`${user.username} | #${user.discriminator}`)
       .addField("User ID:", `${user.id}`, true)
-      .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'Nothing'}`, true)
-      .addField("Account made on:", `${user.createdAt}`)
-      .addField("Joined server:", `${member.joinedAt}`)
+      .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : '*Geen nickname*'}`, true)
+      .addField("Account gemaakt:", `${user.createdAt}`)
+      .addField("Server gejoined:", `${member.joinedAt}`)
       .addBlankField(true)
-      .addField("Presence:", `${user.presence.game ? user.presence.game.name : 'Nothing'}`, true)
-      .addField("Roles:", member.roles.map(roles => `${roles.name}`).join(', '), true)
+      .addField("Status:", `${user.presence.game ? user.presence.game.name : '*Geen presence*'}`, true)
+      .addField("Rollen:", member.roles.map(roles => `${roles.name}`).join(', '), true)
     msg.channel.send({embed});
   }
+  
+  else if (command === "nick") {
+	if ( !args[0] ) {
+		return msg.reply("mention een gebruiker op deze server.");
+	}
+	else if (msg.member.roles.some(r=>["Beheerders"].includes(r.name)) ) {
+		var userID = args[0].replace('<@', '').replace('>', '').replace('!', '');
+		var newNick = args[1];
+		msg.guild.members.get(userID).setNickname(newNick, "nick command executed");
+		msg.reply("prima. De nickname van de gebruiker is nu " + args[1] + ".");
+  }
+	else if (!msg.member.roles.some(r=>["*"].includes(r.name)) ) {
+      return msg.reply("leuke poging. Alleen Beheerders mogen dit.");
+	}
+  }
+  
+  else if (command === "random") {
+	if ( args[0] ) {
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max));
+	}
+	
+	msg.reply((getRandomInt(args[0])) + " is je willekeurige getal.")
+	}
+	else {
+		msg.reply("geef een maximum op om een willekeurig getal te picken.");
+	}
+  }
+  
+  else if (command === "hibp") {
+	search(args[0])
+		.then(data => {
+			if (data.breaches || data.pastes) {
+				msg.reply("oei, de HIBP database geeft datalekken aan voor " + args[0] + "! Ga naar **https://www.haveibeenpwned.com** en check je emailadres daar voor meer informatie!");
+			} else {
+				msg.channel.send('Goed bezig! Geen datalekken gevonden voor ' + args[0] + '.');
+			}
+		})
+		.catch(err => {
+			console.log(err.message);
+		});
+  }
 
+  else if (command === "hond") {
+    dogPicture()
+      .then(directLink => {
+        let embed = new Discord.RichEmbed()
+
+        .setAuthor("Axia")
+        .setTitle("Hond")
+        .setThumbnail(directLink)
+
+        msg.channel.send({embed});
+
+        })
+  }
+
+	else if (command === "") {
+	msg.reply("dit commando bestaat niet. Probeer !help.");
+  
+  }
   else if (command === "help") {
     let embed = new Discord.RichEmbed()
-      .setAuthor("Lizard", "https://cdn.discordapp.com/avatars/469771164521201665/6d3f9681b6e94e961b61b076a5f3b989.png?size=2048", "https://www.twitter.com/JustEtern4l")
-      .setTitle("Commands:")
-      .setDescription(">help | Show this list \n>clear | Delete a given amount of chat messages \n>ping | Calculate the latency \n>pong | Calculate the latency \n>author | Show information about my engineer \n>avatar | Recieve your own (or someone else's) discord avatar \n>8ball | Call in the voodoo gods \n>invite | Recieve my invite link in DM \n>toss | Toss a coin \n>kick | Kick a member from the server \n>ban | Ban a member from the server\n>meme (subject) | Sends a meme about the given subject\n>cat | Sends a random cat picture\n>food | The best food pictures the world has ever seen\n>userinfo (optional: @name#1234)| Get a userinfo tab about yourself or a mentioned user")
+      .setAuthor("Axia")
+      .setTitle("Commando's:")
+      .setDescription("!help | Deze embed \n!clear | Verwijder een groot aantal berichten \n!hibp (email adres) | Check of je voorkomt in datalekken \n!ping | Krijg de ping van de bot \n!nick (gebruiker) (nickname) | Verander een nickname \n!pong | Krijg de ping van de bot \n!random (max) | Krijg een willekeurig nummer onder (max) \n!author | Wie heeft mij gemaakt? \n!avatar | Krijg jouw eigen (of andermans) avatar \n!8ball | Voodoo \n\n!toss | Gooi een muntje \n!kick | Kick een Leerling \n!ban | Ban een Leerling\n!meme (zoekterm) | Zoek voor memes met (zoekterm)\n!kat | Kattenplaatjes!\n!voedsel | Het beste eten wat er is\n!userinfo (optioneel: @naam#1234)| Verkrijg jouw (of andermans) userinfo\n!hond | Hondenplaatjes!")
       .setColor(3604192)
-      .addField("Changelog", "+ Added >userinfo\n* Fixed 19 buggs")
-      .addField("Provide feedback:", "Because i'm always being updated, my engineer would love to hear your feedback. Send a DM on Discord to `Etern4lSec#7923` to provide him with feedback & suggestions.")
-      .addField("In the next update:", "The next update will be a buggpatch\nNext version: V1.0.5")
       .setTimestamp()
-      .setFooter("© Etern4lSec // JustEtern4l // Youri Kuilman 2018");
+      .setFooter("© (Copyright) Youri Kuilman 2018");
 	  
-	  msg.channel.send({embed})
+	  msg.channel.send({embed});
   }
   else {
-	  msg.channel.send("This command couldn't be found. Use `>help` for a list of commands.");
+	  msg.reply("dit commando bestaat niet. Probeer !help.");
   }
 
 });
